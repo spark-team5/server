0b70c05badae6fa52be8a51fe826f6b6dd016691
10Hyuna <kimha105@naver.com>
2025-02-19 01:50:05 +0900

    feat: Request Form DTO 생성

--- src/main/java/com/bamboo/log/diary/dto/request/CreateDiaryRequest.java
+++ src/main/java/com/bamboo/log/diary/dto/request/CreateDiaryRequest.java
@@ -1,4 +1,9 @@
 package com.bamboo.log.diary.dto.request;
 
-public record CreateDiaryRequest() {
+import jakarta.validation.constraints.NotEmpty;
+
+import java.util.List;
+
+public record CreateDiaryRequest(@NotEmpty(message = "User's Id shouldn't be empty") String userId,
+                                 @NotEmpty(message = "Diary Description shouldn't be empty") String diaryDetail) {
 }
