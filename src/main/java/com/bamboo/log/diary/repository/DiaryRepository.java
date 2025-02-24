94e18ec65308070bf84d267211c7ee2d1f60d824
HyunA <129165742+10Hyuna@users.noreply.github.com>
2025-02-24 14:32:00 +0900

    Revert "[Feature] 일기 생성 API"

--- src/main/java/com/bamboo/log/diary/repository/DiaryRepository.java
+++
@@ -1,7 +0,0 @@
-package com.bamboo.log.diary.repository;
-
-import com.bamboo.log.diary.domain.Diary;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface DiaryRepository extends JpaRepository<Diary, Long> {
-}
