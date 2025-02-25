618860f7cfdb7068ec2c69ceab863ebc3b1f334c
10Hyuna <kimha105@naver.com>
2025-02-25 13:18:56 +0900

    Revert "Merge pull request #14 from spark-team5/feature/issue-10"
    This reverts commit 600ac38aa890908eb9bdb2d0b8c0542ec71f9480, reversing
    changes made to 334d9f91ee680862e22558b02a4c7785bdf4aa06.

--- src/main/java/com/bamboo/log/diary/dto/request/CreateDiaryRequest.java
+++
@@ -1,9 +0,0 @@
-package com.bamboo.log.diary.dto.request;
-
-import jakarta.validation.constraints.NotEmpty;
-
-import java.util.List;
-
-public record CreateDiaryRequest(@NotEmpty(message = "User's Id shouldn't be empty") String userId,
-                                 @NotEmpty(message = "Diary Description shouldn't be empty") String diaryDetail) {
-}
--- src/main/java/com/bamboo/log/diary/repository/TodaySummaryRepository.java
+++
@@ -1,7 +0,0 @@
-package com.bamboo.log.diary.repository;
-
-import com.bamboo.log.diary.domain.TodaySummary;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TodaySummaryRepository extends JpaRepository<TodaySummary, Long> {
-}
