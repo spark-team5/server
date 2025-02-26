0af3cc295bd07534f1f3bee9597f36050f9ee24c
10Hyuna <kimha105@naver.com>
2025-02-26 22:07:29 +0900

    Fix: Merge conflict 해결 in DiaryService

--- src/main/java/com/bamboo/log/diary/service/diary/DiaryServiceImpl.java
+++ src/main/java/com/bamboo/log/diary/service/diary/DiaryServiceImpl.java
@@ -1,6 +1,7 @@
 package com.bamboo.log.diary.service.diary;
 
 import com.bamboo.log.diary.domain.Diary;
+import com.bamboo.log.diary.domain.TodaySummary;
 import com.bamboo.log.diary.dto.ParseYearMonth;
 import com.bamboo.log.diary.dto.request.CreateDiaryRequest;
 import com.bamboo.log.diary.dto.response.CreateDiaryResponse;
@@ -8,12 +9,10 @@ import com.bamboo.log.diary.dto.response.GetDiariesOfMonthResponse;
 import com.bamboo.log.diary.dto.response.GetDiariesOfMonthResponse.DiaryOfMonth;
 import com.bamboo.log.diary.repository.DiaryRepository;
 import com.bamboo.log.diary.repository.TodaySummaryRepository;
-import com.bamboo.log.domain.user.oauth.repository.UserRepository;
-
 import com.bamboo.log.diary.service.summary.TodaySummaryService;
 import com.bamboo.log.domain.user.jwt.service.UserContextUtil;
-import com.bamboo.log.domain.user.oauth.dto.KakaoResponse;
 import com.bamboo.log.domain.user.oauth.entity.UserEntity;
+import com.bamboo.log.domain.user.oauth.repository.UserRepository;
 import com.bamboo.log.utils.ResponseHandler;
 import com.bamboo.log.utils.dto.ResponseForm;
 import lombok.RequiredArgsConstructor;
@@ -27,6 +26,7 @@ import java.time.YearMonth;
 import java.time.format.DateTimeFormatter;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Service
@@ -42,15 +42,10 @@ public class DiaryServiceImpl implements DiaryService {
 
     @Override
     public ResponseEntity createDiary(CreateDiaryRequest createDiaryRequest) {
-        // 해당 시점의 localDateTime 저장
         LocalDateTime localDateTime = LocalDateTime.now();
-
-        // 데이터베이스에 저장
         Diary diary = saveDiary(createDiaryRequest, localDateTime);
-
         byte[] todayImage = todaySummaryService.createTodaySummaryImage(createDiaryRequest.diaryDetail());
 
-        // todaySummaryImage 데이터베이스에 저장
         try {
             todaySummaryService.saveTodaySummaryImage(todayImage, diary.getId());
         } catch (RuntimeException e) {
@@ -59,7 +54,6 @@ public class DiaryServiceImpl implements DiaryService {
             return ResponseHandler.create500Error(new ResponseForm(), e);
         }
 
-        // 201 code 반환
         return ResponseHandler.create201Response(new ResponseForm(), new CreateDiaryResponse(localDateTime));
     }
 
@@ -69,14 +63,13 @@ public class DiaryServiceImpl implements DiaryService {
         UserEntity user = userRepository.findByUsername(userContextUtil.getUsername());
 
         try {
-            List<Diary> diaries =
-                    diaryRepository.findByUserAAndCreatedAtBetween(user,
-                            parseYearMonth.getStartOfMonth(), parseYearMonth.getEndOfMonth());
+            List<Diary> diaries = diaryRepository.findByUserAAndCreatedAtBetween(user,
+                    parseYearMonth.getStartOfMonth(), parseYearMonth.getEndOfMonth());
 
             List<Long> diaryIds = diaries.stream().map(Diary::getId).toList();
             Map<Long, byte[]> summaryImageMap = todaySummaryRepository.findByDiaryIdIn(diaryIds)
                     .stream()
-                    .collect(Collectors.toMap(summary -> summary.getDiaryId(), summary -> summary.getImageData()));
+                    .collect(Collectors.toMap(TodaySummary::getDiaryId, TodaySummary::getImageData));
 
             List<DiaryOfMonth> diaryOfMonthList = diaries.stream()
                     .map(diary -> new DiaryOfMonth(
@@ -94,6 +87,33 @@ public class DiaryServiceImpl implements DiaryService {
                             .build());
         } catch (RuntimeException e) {
             return ResponseHandler.create404Error(new ResponseForm(), e);
+        } catch (Exception e) {
+            return ResponseHandler.create500Error(new ResponseForm(), e);
+        }
+    }
+
+    @Override
+    public ResponseEntity getDiaryByDate(LocalDateTime date) {
+        UserEntity user = userRepository.findByUsername(userContextUtil.getUsername());
+
+        try {
+            List<Diary> diaries = diaryRepository.findByUserAAndCreatedAtBetween(user, date, date);
+            if (diaries.isEmpty()) {
+                throw new RuntimeException("해당 날짜에 작성된 일기가 없습니다.");
+            }
+
+            Diary diaryByDate = diaries.get(0);
+            Optional<TodaySummary> summaryImage = todaySummaryRepository.findByDiaryId(diaryByDate.getId());
+
+            return ResponseHandler.create200Response(new ResponseForm(),
+                    CheckDiaryResponse.builder()
+                            .date(diaryByDate.getCreatedAt())
+                            .diaryDescription(diaryByDate.getContext())
+                            .summaryImage(summaryImage.map(TodaySummary::getImageData).orElse(null))
+                            .build());
+
+        } catch (RuntimeException e) {
+            return ResponseHandler.create404Error(new ResponseForm(), new IllegalArgumentException("해당 날짜에 작성된 일기가 없습니다."));
         } catch (Exception e) {
             return ResponseHandler.create500Error(new ResponseForm(), e);
         }
@@ -122,5 +142,4 @@ public class DiaryServiceImpl implements DiaryService {
                 .endOfMonth(endOfMonth)
                 .build();
     }
-
 }
