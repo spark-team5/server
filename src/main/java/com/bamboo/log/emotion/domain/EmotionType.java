06f9be4b765bffff51aa9fd232db07ee110fc83e
qhdudedi <boyoung035@gmail.com>
2025-02-26 23:05:32 +0900

    Feat: EmotionType 생성
    - 서버 오류 시, NONE 리턴

---
+++ src/main/java/com/bamboo/log/emotion/domain/EmotionType.java
@@ -0,0 +1,10 @@
+package com.bamboo.log.emotion.domain;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public enum EmotionType {
+    ANGRY,  HAPPY, NEUTRAL, SAD, NONE ;
+}
