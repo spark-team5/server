8ba6864561715d67efaa006b7b45161356919b08
qhdudedi <boyoung035@gmail.com>
2025-02-26 23:02:55 +0900

    Fix: 표정 분석 위한 response 수정
    - 좌표 값 고려하여 List으로 변경

--- src/main/java/com/bamboo/log/emotion/dto/FaceDetectionResponse.java
+++ src/main/java/com/bamboo/log/emotion/dto/FaceDetectionResponse.java
@@ -2,12 +2,14 @@ package com.bamboo.log.emotion.dto;
 
 import org.springframework.http.HttpStatus;
 
+import java.util.List;
+
 public record FaceDetectionResponse(
         int statusCode,
         String statusMessage,
-        String message
+        List<BoundingBox> faceBox
 ) {
-    public FaceDetectionResponse(HttpStatus httpStatus, String message) {
-        this(httpStatus.value(), httpStatus.name(), message);
+    public FaceDetectionResponse(HttpStatus httpStatus, List<BoundingBox> faceBox) {
+        this(httpStatus.value(), httpStatus.name(), faceBox);
     }
 }
