73f80ab63182f46491e382bb6b9f95c3b5faf7b3
heodongun <heodongun0922@gmail.com>
2025-02-28 04:31:48 +0900

    feature : 죄송합니다...

--- src/main/java/com/bamboo/log/common/config/CorsMvcConfig.java
+++ src/main/java/com/bamboo/log/common/config/CorsMvcConfig.java
@@ -10,7 +10,7 @@ public class CorsMvcConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry corsRegistry) {
         corsRegistry.addMapping("/**")
-                .allowedOrigins("*") // 모든 Origin 허용
+                .allowedOriginPatterns("*")
                 .allowedMethods("*") // 모든 HTTP 메서드 허용
                 .allowedHeaders("*") // 모든 헤더 허용
                 .allowCredentials(true) // 쿠키 허용
--- src/main/java/com/bamboo/log/common/config/SecurityConfig.java
+++ src/main/java/com/bamboo/log/common/config/SecurityConfig.java
@@ -63,7 +63,7 @@ public class SecurityConfig {
             public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
                 CorsConfiguration configuration = new CorsConfiguration();
                 //프론트 url 넣기
-                configuration.setAllowedOrigins(Collections.singletonList("http://localhost:3000"));
+                configuration.setAllowedOrigins(Collections.singletonList("*"));
                 configuration.setAllowedMethods(Collections.singletonList("*"));
                 configuration.setAllowCredentials(true);
                 configuration.setAllowedHeaders(Collections.singletonList("*"));
