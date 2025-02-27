28ec3637436159633c89f469c4ba321fc23c5e6a
qhdudedi <boyoung035@gmail.com>
2025-02-28 01:59:28 +0900

    Feat: 죽순이 분양 로직 구현

--- src/main/java/com/bamboo/log/juksoon/service/JuksoonService.java
+++ src/main/java/com/bamboo/log/juksoon/service/JuksoonService.java
@@ -3,5 +3,6 @@ package com.bamboo.log.juksoon.service;
 import com.bamboo.log.juksoon.dto.JuksooniResponse;
 
 public interface JuksoonService {
+    JuksooniResponse adoptJuksooni(Long userId);
     JuksooniResponse checkJuksooniStatus(Long userId);
 }
